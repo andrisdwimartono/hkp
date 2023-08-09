c6a256239a503144a39e59c4f84c5106d8e15c8f
Andris Dwi Martono <andrisdwimartono@gmail.com>
2023-08-09 04:12:41 +0000

    Laporan LKP
    andrisdwimartono

--- erpnext/projects/doctype/laporan_mingguan_tool/laporan_mingguan_tool.js
+++ erpnext/projects/doctype/laporan_mingguan_tool/laporan_mingguan_tool.js
@@ -1,17 +1,30 @@
 // Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
-
-
+zoomout();
 frappe.ui.form.on('Laporan Mingguan Tool', {
-	
+	validate: function(frm){
+		
+	},
 	refresh: function(frm) {
-		frm.set_value("sub_contract_hand_over", "SPK-2023-000001");
-		frm.refresh_field("sub_contract_hand_over");
+		// frm.set_value("sub_contract_hand_over", "SPK-2023-000003");
+		// frm.refresh_field("sub_contract_hand_over");
 
 		//$(frm.fields_dict.abc.wrapper).html("<table><tr><td>ss</td></tr></table>aaa");
 		//frm.set_value("result_html", "<table><tr><td>ss</td></tr></table>aaa");
 		//frm.refresh_field("result_html");
-		
+		frm.doc.show_submit = false;
+		//frm.doc.show_save = false;
+		frm.disable_save();
+		frm.add_custom_button('Simpan', function () {
+			frm.trigger("updating");
+		});
+		frm.add_custom_button('<svg class="icon  icon-sm" style=""><use class="" href="#icon-printer"></use></svg>', function () {
+			if(frm.doc.sub_contract_weekly_progress){
+				window.location.href = "/printview?doctype=Sub%20Contract%20Weekly%20Progress&name="+frm.doc.sub_contract_weekly_progress+"&format=LKP%20Desgin&no_letterhead=0&letterhead=KOP%20HKP&settings=%7B%7D&_lang=id";
+			}
+			
+		});
+
 		if(frm.doc.posting_date && !frm.doc.week){
 			frappe.call({
 				method: 'erpnext.projects.doctype.sub_contract_weekly_progress.sub_contract_weekly_progress.check_week',
@@ -77,8 +90,8 @@ frappe.ui.form.on('Laporan Mingguan Tool', {
 				'sub_contract_hand_over': frm.doc.sub_contract_hand_over
 			},
 			callback: function(r) {
+				frm.clear_table('sub_contract_weekly_progress_detail');
 				if(r.message && r.message.length > 0){
-					frm.clear_table('sub_contract_weekly_progress_detail');
 					for(var i = 0; i < r.message.length; i++){
 						frm.add_child('sub_contract_weekly_progress_detail', {
 							job_detail: r.message[i].job_detail,
@@ -90,11 +103,16 @@ frappe.ui.form.on('Laporan Mingguan Tool', {
 							volume_cumulative_last_week: r.message[i].volume_cumulative_last_week,
 							vol_this_week: r.message[i].vol_this_week,
 							volume_cumulative_thist_week: r.message[i].volume_cumulative_thist_week,
-							deviasi: r.message[i].deviasi
+							deviasi : r.message[i].volume_plan_from_last_week-r.message[i].vol_this_week
 						});
+						// $("div[data-fieldname='deviasi']").append("<input value='xxxxx' disabled class='input-with-feedback form-control input-sm'>");
+						// $("div[data-fieldname='deviasi']").removeClass("col-xs-1");
+						// $('[data-fieldname="sub_contract_weekly_progress_detail"]').html("aasdsss");
+						//$('[data-fieldname="job_detail"]').html("aasdsss");
 					}
-					frm.refresh_field('sub_contract_weekly_progress_detail');
 				}
+				frm.refresh_field('sub_contract_weekly_progress_detail');
+				zoomout();
 			}
 		});
 	},
@@ -126,4 +144,92 @@ frappe.ui.form.on('Laporan Mingguan Tool', {
 		
 		frm.doc.show_submit = false;
 	},
+
+	updating: function(frm){
+		var x = [];
+		for(var i = 0; i < frm.doc.sub_contract_weekly_progress_detail.length; i ++){
+			x.push({
+				"name":frm.doc.sub_contract_weekly_progress_detail[i].name?frm.doc.sub_contract_weekly_progress_detail[i].name:null,
+				"idx": frm.doc.idx,
+				"job_detail":frm.doc.sub_contract_weekly_progress_detail[i].job_detail,
+				"uom":frm.doc.sub_contract_weekly_progress_detail[i].uom,
+				"volume":frm.doc.sub_contract_weekly_progress_detail[i].volume,
+				"weight":frm.doc.sub_contract_weekly_progress_detail[i].weight,
+				"volume_plan_from_last_week":frm.doc.sub_contract_weekly_progress_detail[i].volume_plan_from_last_week,
+				"vol_next_week_plan":frm.doc.sub_contract_weekly_progress_detail[i].vol_next_week_plan,
+				"volume_cumulative_last_week":frm.doc.sub_contract_weekly_progress_detail[i].volume_cumulative_last_week,
+				"vol_this_week":frm.doc.sub_contract_weekly_progress_detail[i].vol_this_week,
+				"volume_cumulative_thist_week":frm.doc.sub_contract_weekly_progress_detail[i].volume_cumulative_thist_week,
+				"deviasi":frm.doc.sub_contract_weekly_progress_detail[i].deviasi
+			});
+		}
+		frappe.call({
+			method: 'erpnext.projects.doctype.sub_contract_weekly_progress.sub_contract_weekly_progress.saving',
+			args: {
+				"name":frm.doc.name?frm.doc.name:null,
+				"sub_contract_hand_over":frm.doc.sub_contract_hand_over,
+				"project":frm.doc.project,
+				"sub_contract":frm.doc.sub_contract,
+				"job_name":frm.doc.job_name,
+				"posting_date":frm.doc.posting_date,
+				"week":frm.doc.week,
+				"contractor_name":frm.doc.contractor_name,
+				"project_name":frm.doc.project_name,
+				"pic_name":frm.doc.pic_name,
+				"budget_amount":frm.doc.budget_amount,
+				"sub_contract_weekly_progress":frm.doc.sub_contract_weekly_progress,
+				"sub_contract_weekly_progress_last_week":frm.doc.sub_contract_weekly_progress_last_week,
+				"sub_contract_weekly_progress_detail":x
+			},
+			callback: function(r) {
+				if(r.message){
+					var vals = r.message;
+					frm.set_value("sub_contract_weekly_progress", vals);
+					frm.refresh_field("sub_contract_weekly_progress");
+					msgprint("Berhasil disimpan");
+				}
+			}
+		});
+	},
 });
+
+
+frappe.ui.form.on("Sub Contract Weekly Progress Details", {
+	sub_contract_weekly_progress_detail_add: function(frm, dt, dn) {
+        //console.log('added!');
+		// $('[data-fieldname="job_detail"][title="Uraian Pekerjaan"]').css("font-size", "10px");
+		zoomout();
+		
+		
+		//row-index
+    },
+	vol_this_week: function(frm, dt, dn) {
+		var d = locals[dt][dn];
+		d.deviasi = d.volume_plan_from_last_week-d.vol_this_week;
+		frm.refresh_field('sub_contract_weekly_progress_detail');
+		d.volume_cumulative_thist_week = d.volume_cumulative_last_week+d.vol_this_week;
+		frm.refresh_field('volume_cumulative_thist_week');
+	},
+	volume_plan_from_last_week: function(frm, dt, dn) {
+		var d = locals[dt][dn];
+		d.deviasi = d.volume_plan_from_last_week-d.vol_this_week;
+		frm.refresh_field('sub_contract_weekly_progress_detail');
+		d.volume_cumulative_thist_week = d.volume_cumulative_last_week+d.vol_this_week;
+		frm.refresh_field('volume_cumulative_thist_week');
+	}
+});
+
+function zoomout(){
+	$('.grid-static-col').css("font-size", "10px");
+	$('.row-index').css("font-size", "10px");
+	$('[data-doctype="Sub Contract Weekly Progress Details"]').css("font-size", "10px");
+
+	//ganti label
+	$('.grid-heading-row>.grid-row>.data-row>[data-fieldname="volume"]>.static-area').html("Volume");
+	$('.grid-heading-row>.grid-row>.data-row>[data-fieldname="uom"]>.static-area').html("Satuan");
+	$('.grid-heading-row>.grid-row>.data-row>[data-fieldname="volume_plan_from_last_week"]>.static-area').html("<div>Rencana</div><div>Minggu Ini</div>");
+	$('.grid-heading-row>.grid-row>.data-row>[data-fieldname="vol_next_week_plan"]>.static-area').html("<div>Rencana</div><div>Minggu Depan</div>");
+	$('.grid-heading-row>.grid-row>.data-row>[data-fieldname="volume_cumulative_last_week"]>.static-area').html("<div>Kumulatif</div><div>Minggu Lalu</div>");
+	$('.grid-heading-row>.grid-row>.data-row>[data-fieldname="vol_this_week"]>.static-area').html("<div>Realisasi</div><div>Minggu Ini</div>");
+	$('.grid-heading-row>.grid-row>.data-row>[data-fieldname="volume_cumulative_thist_week"]>.static-area').html("<div>Kumulatif</div><div>Minggu Ini</div>");
+}
--- erpnext/projects/doctype/sub_contract_weekly_progress/sub_contract_weekly_progress.py
+++ erpnext/projects/doctype/sub_contract_weekly_progress/sub_contract_weekly_progress.py
@@ -4,9 +4,26 @@
 import frappe
 from frappe.model.document import Document
 from datetime import datetime, timedelta
+import json
 
 class SubContractWeeklyProgress(Document):
-	pass
+	def validate(self):
+		lists = frappe.db.sql("""SELECT * FROM `tabSub Contract Weekly Progress` a
+			WHERE a.week > '{0}' AND a.sub_contract_hand_over = '{1}'""".format(self.week, self.sub_contract_hand_over), as_dict=1)
+		for li in lists:
+			last_week = int(li.week or 0)-1
+			dts = frappe.db.sql("""SELECT b.name, h.volume_cumulative_last_week FROM `tabSub Contract Weekly Progress` a
+				INNER JOIN `tabSub Contract Weekly Progress Detail` b ON b.parent = a.name
+				LEFT JOIN (SELECT g.job_detail, SUM(g.vol_this_week) volume_cumulative_last_week FROM `tabSub Contract Weekly Progress` f
+					INNER JOIN `tabSub Contract Weekly Progress Detail` g ON g.parent = f.name
+					WHERE f.week <= '{2}' AND f.sub_contract_hand_over = '{1}'
+					GROUP BY g.job_detail
+					) h ON h.job_detail = b.job_detail
+				WHERE a.name = '{3}'""".format(li.week, li.sub_contract_hand_over, last_week, li.name), as_dict=1)
+			for dt in dts:
+				frappe.db.sql("""UPDATE `tabSub Contract Weekly Progress Detail` 
+				SET volume_cumulative_last_week = '{1}'
+				WHERE name = '{0}'""".format(dt.name, dt.volume_cumulative_last_week))
 	
 	# def validate(self):
 	# 	d1 = datetime.strptime("{0}-01-01".format(datetime.now().year), "%Y-%m-%d")
@@ -54,10 +71,120 @@ def get_sub_contract_weekly_progress(week, sub_contract_hand_over):
 @frappe.whitelist()
 def get_sub_contract_weekly_progress_detail(week, sub_contract_hand_over):
 	if sub_contract_hand_over and week:
-		return frappe.db.sql("""
-       	SELECT b.job_detail, b.uom, b.volume, b.weight, b.volume_plan_from_last_week, b.vol_next_week_plan, b.volume_cumulative_last_week, b.vol_this_week, b.volume_cumulative_thist_week, b.deviasi FROM `tabSub Contract Weekly Progress` a
+		last_week = int(week)-1
+		a = frappe.db.sql("""
+       	SELECT b.job_detail, b.uom, b.volume, b.weight, e.vol_next_week_plan volume_plan_from_last_week, b.vol_next_week_plan, h.volume_cumulative_last_week, b.vol_this_week, k.volume_cumulative_thist_week, 0 deviasi FROM `tabSub Contract Weekly Progress` a
 		INNER JOIN `tabSub Contract Weekly Progress Detail` b ON b.parent = a.name
+		LEFT JOIN (SELECT d.* FROM `tabSub Contract Weekly Progress` c
+			INNER JOIN `tabSub Contract Weekly Progress Detail` d ON d.parent = c.name
+			WHERE c.week = '{2}' AND c.sub_contract_hand_over = '{1}'
+			GROUP BY d.job_detail
+			) e ON e.job_detail = b.job_detail
+		LEFT JOIN (SELECT g.job_detail, SUM(g.vol_this_week) volume_cumulative_last_week FROM `tabSub Contract Weekly Progress` f
+			INNER JOIN `tabSub Contract Weekly Progress Detail` g ON g.parent = f.name
+			WHERE f.week <= '{2}' AND f.sub_contract_hand_over = '{1}'
+			GROUP BY g.job_detail
+			) h ON h.job_detail = b.job_detail
+		LEFT JOIN (SELECT j.job_detail, SUM(j.vol_this_week) volume_cumulative_thist_week FROM `tabSub Contract Weekly Progress` i
+			INNER JOIN `tabSub Contract Weekly Progress Detail` j ON j.parent = i.name
+			WHERE i.week <= '{0}' AND i.sub_contract_hand_over = '{1}'
+			GROUP BY j.job_detail
+			) k ON k.job_detail = b.job_detail
 		WHERE a.week = '{0}' AND a.sub_contract_hand_over = '{1}'
-        """.format(week, sub_contract_hand_over), as_dict=1)
+        """.format(week, sub_contract_hand_over, last_week), as_dict=1)
+		if a:
+			return a
+		else:
+			return frappe.db.sql("""
+			SELECT b.job_detail, b.uom, b.volume, b.weight, b.vol_next_week_plan volume_plan_from_last_week, 0 vol_next_week_plan, h.volume_cumulative_last_week, 0 vol_this_week, k.volume_cumulative_thist_week, 0 deviasi FROM `tabSub Contract Weekly Progress` a
+			INNER JOIN `tabSub Contract Weekly Progress Detail` b ON b.parent = a.name
+			LEFT JOIN (SELECT g.job_detail, SUM(g.vol_this_week) volume_cumulative_last_week FROM `tabSub Contract Weekly Progress` f
+				INNER JOIN `tabSub Contract Weekly Progress Detail` g ON g.parent = f.name
+				WHERE f.week <= '{2}' AND f.sub_contract_hand_over = '{1}'
+				GROUP BY g.job_detail
+				) h ON h.job_detail = b.job_detail
+			LEFT JOIN (SELECT j.job_detail, SUM(j.vol_this_week) volume_cumulative_thist_week FROM `tabSub Contract Weekly Progress` i
+				INNER JOIN `tabSub Contract Weekly Progress Detail` j ON j.parent = i.name
+				WHERE i.week <= '{0}' AND i.sub_contract_hand_over = '{1}'
+				GROUP BY j.job_detail
+				) k ON k.job_detail = b.job_detail
+			WHERE a.week = '{2}' AND a.sub_contract_hand_over = '{1}'
+			""".format(week, sub_contract_hand_over, last_week), as_dict=1)
 	return None
 
+@frappe.whitelist()
+def saving(name, sub_contract_hand_over, project, sub_contract, job_name, posting_date, week, contractor_name, project_name, pic_name, budget_amount, sub_contract_weekly_progress, sub_contract_weekly_progress_last_week, sub_contract_weekly_progress_detail):
+	aaa = json.loads(sub_contract_weekly_progress_detail)
+	nm = check_is_exist(name, sub_contract_hand_over, project, sub_contract, job_name, posting_date, week, contractor_name, project_name, pic_name, budget_amount, sub_contract_weekly_progress, sub_contract_weekly_progress_last_week, sub_contract_weekly_progress_detail)
+	if nm:
+		scwp = frappe.get_doc("Sub Contract Weekly Progress", nm)
+		scwp.sub_contract_hand_over = sub_contract_hand_over
+		scwp.project = project
+		scwp.sub_contract = sub_contract
+		scwp.job_name = job_name
+		scwp.posting_date = posting_date
+		scwp.week = week
+		scwp.contractor_name = contractor_name
+		scwp.project_name = project_name
+		scwp.pic_name = pic_name
+		scwp.budget_amount = budget_amount
+		
+		for scwpd in scwp.sub_contract_weekly_progress_detail:
+			frappe.delete_doc("Sub Contract Weekly Progress Detail", scwpd.name)
+		for n in aaa:
+			scwp.append("sub_contract_weekly_progress_detail", {
+				"idx": n["idx"],
+				"job_detail": n["job_detail"],
+				"uom": n["uom"],
+				"volume": n["volume"],
+				"weight": n["weight"],
+				"volume_plan_from_last_week": n["volume_plan_from_last_week"],
+				"vol_next_week_plan": n["vol_next_week_plan"],
+				"volume_cumulative_last_week": n["volume_cumulative_last_week"],
+				"vol_this_week": n["vol_this_week"],
+				"volume_cumulative_thist_week": n["volume_cumulative_thist_week"],
+				"deviasi": n["deviasi"]
+			})
+		scwp.save()
+
+		return scwp.name
+	else:
+		scwp = frappe.get_doc({
+			"doctype": "Sub Contract Weekly Progress",
+			"sub_contract_hand_over": sub_contract_hand_over,
+			"project": project,
+			"sub_contract": sub_contract,
+			"job_name": job_name,
+			"posting_date": posting_date,
+			"week": week,
+			"contractor_name": contractor_name,
+			"project_name": project_name,
+			"pic_name": pic_name,
+			"budget_amount": budget_amount,
+		})
+		
+		for n in aaa:
+			scwp.append("sub_contract_weekly_progress_detail", {
+				"job_detail": n["job_detail"],
+				"uom": n["uom"],
+				"volume": n["volume"],
+				"weight": n["weight"],
+				"volume_plan_from_last_week": n["volume_plan_from_last_week"],
+				"vol_next_week_plan": n["vol_next_week_plan"],
+				"volume_cumulative_last_week": n["volume_cumulative_last_week"],
+				"vol_this_week": n["vol_this_week"],
+				"volume_cumulative_thist_week": n["volume_cumulative_thist_week"],
+				"deviasi": n["deviasi"]
+			})
+		scwp.insert(ignore_permissions=True)
+		return scwp.name
+
+def check_is_exist(name, sub_contract_hand_over, project, sub_contract, job_name, posting_date, week, contractor_name, project_name, pic_name, budget_amount, sub_contract_weekly_progress, sub_contract_weekly_progress_last_week, sub_contract_weekly_progress_detail):
+	aa = frappe.db.sql("""
+	SELECT * FROM `tabSub Contract Weekly Progress` a 
+	WHERE a.week = '{0}' AND a.sub_contract_hand_over = '{1}'""".format(week, sub_contract_hand_over), as_dict=1)
+	if aa:
+		return aa[0].name
+	else:
+		return None
+	
