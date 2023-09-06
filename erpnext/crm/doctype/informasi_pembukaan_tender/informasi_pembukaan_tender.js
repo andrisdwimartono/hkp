86f94b1fc200a7b9412e9bb44a6dcea4cca737f0
Andris Dwi Martono <andrisdwimartono@gmail.com>
2023-09-06 17:03:46 +0000

    Informasi Pembukaan Tender
    andrisdwimartono

--- erpnext/crm/doctype/informasi_pembukaan_tender/informasi_pembukaan_tender.js
+++ erpnext/crm/doctype/informasi_pembukaan_tender/informasi_pembukaan_tender.js
@@ -4,10 +4,10 @@
 frappe.ui.form.on('INFORMASI PEMBUKAAN TENDER', {
 	refresh: function(frm) {
 		if (frm.doc.__unsaved == 1)	{
-			if(!frm.doc.hasil_pembukaan_tender){
-				frm.clear_table("hasil_pembukaan_tender");
+			if(!frm.doc.process_rules){
+				frm.clear_table("process_rules");
 				frappe.call({
-					method: 'erpnext.crm.utils.get_hasil_pembukaan_tender',
+					method: 'erpnext.projects.utils.get_process_rules',
 					args: {
 						'doctype': frm.doc.doctype
 					},
@@ -15,21 +15,47 @@ frappe.ui.form.on('INFORMASI PEMBUKAAN TENDER', {
 						if(r.message){
 							var vals = r.message;
 							for(var i = 0; i < vals.length; i++){
-								var pr = frm.add_child("hasil_pembukaan_tender");
-								// pr.jabatan = vals[i].jabatan;
-								// pr.jabatan_abbr = vals[i].jabatan_abbr;
-								// pr.state = vals[i].state;
-								// pr.employee = vals[i].employee;
-								// pr.employee_name = vals[i].employee_name;
-								// pr.user = vals[i].user;
+								var pr = frm.add_child("process_rules");
+								pr.jabatan = vals[i].jabatan;
+								pr.jabatan_abbr = vals[i].jabatan_abbr;
+								pr.state = vals[i].state;
+								pr.employee = vals[i].employee;
+								pr.initial = vals[i].initial;
+								pr.employee_name = vals[i].employee_name;
+								pr.user = vals[i].user;
 							}
-							frm.refresh_field("hasil_pembukaan_tender");
+							frm.refresh_field("process_rules");
 						}
 					}
 				});
 			}
-		} else {
+		}else{
 			
 		}
+		cur_frm.fields_dict['process_rules'].$wrapper.find('.grid-add-row').addClass('d-none');
+	},
+	informasi_pembuatan_penawaran_terkait: function(frm){
+		frm.clear_table("hasil_pembukaan_tender");
+		frappe.call({
+			method: 'erpnext.crm.doctype.informasi_pembukaan_tender.informasi_pembukaan_tender.get_peserta',
+			args: {
+				'informasi_pembuatan_penawaran_terkait': frm.doc.informasi_pembuatan_penawaran_terkait
+			},
+			callback: function(r) {
+				if(r.message){
+					var vals = r.message;
+					for(var i = 0; i < vals.length; i++){
+						var pr = frm.add_child("hasil_pembukaan_tender");
+						pr.nama_peserta = vals[i].peserta;
+						// pr.jabatan_abbr = vals[i].jabatan_abbr;
+						// pr.state = vals[i].state;
+						// pr.employee = vals[i].employee;
+						// pr.employee_name = vals[i].employee_name;
+						// pr.user = vals[i].user;
+					}
+					frm.refresh_field("hasil_pembukaan_tender");
+				}
+			}
+		});
 	}
 });
--- erpnext/crm/doctype/informasi_pembukaan_tender/informasi_pembukaan_tender.py
+++ erpnext/crm/doctype/informasi_pembukaan_tender/informasi_pembukaan_tender.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class INFORMASIPEMBUKAANTENDER(Document):
 	pass
+@frappe.whitelist()
+def get_peserta(informasi_pembuatan_penawaran_terkait):
+	return frappe.db.sql("""SELECT a.* FROM `tabLAPORAN PENJELASAN TENDER _ PESERTA TENDER` a
+			INNER JOIN `tabLAPORAN PENJELASAN TENDER` b ON b.name = a.parent		  
+			WHERE b.informasi_pembuatan_penawaran = '{0}' ORDER BY idx ASC""".format(informasi_pembuatan_penawaran_terkait), as_dict=1)
